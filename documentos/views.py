7e6c0220caf285512b6e294cc5395c0bb61502ab
Breno Rodrigues <breno.dnrodrigues.senai@gmail.com>
2025-10-01 10:41:16 -0300

    Atualização Modelo Usuário (função de retornar nome estava incorreta, e importação da bibliotecxa 'django-countries) e Inicio de criação da página de Digitalização de Documentos

--- documentos/views.py
+++ documentos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Documento
+from django.views.generic import CreateView
 # Create your views here.
+
+class DigitalizarDocumentos(CreateView):
+    model = Documento
+    template_name = 'DigitalizarDocumentos.html'
+    fields = "__all__"
+
