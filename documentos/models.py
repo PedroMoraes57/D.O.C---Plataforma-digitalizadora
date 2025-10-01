44003c956647ad09776dfe75dfa8498548dcb729
Pedro Moraes <pedroferreira5711@gmail.com>
2025-10-01 11:16:39 -0300

    Alterações index e login

--- documentos/models.py
+++ documentos/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from usuarios.models import Usuarios
 class Documentos (models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     tipo = models.CharField(max_length=20, null=False, blank=False) 
     setor = models.CharField(max_length=25, null=False, blank=False)
     assunto = models.CharField(max_length=200)
     data_publicacao = models.DateTimeField(max_length=25, null=False, blank=False, auto_now_add=True)
-    usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True)
+    usuario = models.ForeignKey(Usuarios, on_delete=models.SET_NULL, null=True)
     # Campos extra extraídos via OCR/IA
     prazo = models.DateField(blank=True, null=True)
 
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def pagina_login(request):
+    return render(request, 'usuarios/login.html')
