1a2f9d3c3258f5ba8ce5f18bdd35f6a15d812622
Breno Rodrigues <breno.dnrodrigues.senai@gmail.com>
2025-10-01 09:18:53 -0300

    Atualização Model (criação de campos de foto e país) e novas dependecias instaladas

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django_countries import CountryField
 
 class Usuarios (models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
-    email = models.EmailField(max_length=20, null=False, blank=False, unique=True) 
+    email = models.EmailField(null=False, blank=False, unique=True)
+    pais = CountryField(blank_label=("Selecione um país"))
     telefone = models.CharField(max_length=25, null=False, blank=False)
+    foto_perfil = models.ImageField()
 
     def __str__(self):
         return self.nome
